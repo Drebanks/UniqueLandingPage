442ac0009a18f71b94bce6ef3618c9272d0917ca
Dare Netufo <dareitunu@gmail.com>
2021-10-26 16:04:30 +0100

    nav fixed

--- landpage.js
+++ landpage.js
@@ -1,30 +1,15 @@
-    const menupie = document.getElementsByClassName(".menupie")[0]
-    const hiddenmenu = document.getElementsByClassName(".hiddenmenu")[0]
-    
-    menupie.addEventListener("click", () => {
-        hiddenMenu.classList.toggle("hiddenmenu-active");
-    });
+let menupie = document.querySelector(".menupie");
+let hide = document.querySelector(".hide");
 
-
-
-
-
-// let menu = document.querySelector("#menu-icon");
-// let ulTop = document.querySelector(".ul-top");
-// let remove = document.querySelector(".remove")
-
-
-// let show = false;
-// function popUp() {
-//     console.log(true)
-//   if (show) {
-//     // remove.style.display = "block"
-//     ulTop.style.display = "none";
-//     show= false
-//   } else {
-//     ulTop.style.display = "block";
-//     // remove.style.display = "none"
-//     show = true
-//   }
-// }
-// menu.addEventListener("click", popUp);
+let show = false;
+function popUp() {
+  console.log(true);
+  if (show) {
+    hide.style.display = "none";
+    show = false
+  } else {
+      hide.style.display = "block";
+      show = true
+  }
+}
+menupie.addEventListener("click", popUp);
